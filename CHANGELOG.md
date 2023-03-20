e3f0b933e6386af9b765d4ad28cab6a0de184d6b
Justin Searls <searls@gmail.com>
2023-03-20 08:56:05 -0400

    $ bundle gem standard-rails

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2023-03-20
+
+- Initial release
---
+++ lib/standard/rails.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "rails/version"
+
+module Standard
+  module Rails
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
