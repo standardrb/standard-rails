96491cddb2be27f75e06b99dcc8e89f99a034018
Justin Searls <searls@gmail.com>
2023-04-22 08:09:49 -0400

    nest module definition to stop relying on require order

--- lib/standard/rails.rb
+++ lib/standard/rails.rb
@@ -2,8 +2,3 @@ require "lint_roller"
 
 require_relative "rails/version"
 require_relative "rails/plugin"
-
-module Standard
-  module Rails
-  end
-end
--- lib/standard/rails/plugin.rb
+++ lib/standard/rails/plugin.rb
@@ -1,60 +1,62 @@
 require "yaml"
 
-module Standard::Rails
-  class Plugin < LintRoller::Plugin
-    def initialize(config)
-      @config = config
-    end
+module Standard
+  module Rails
+    class Plugin < LintRoller::Plugin
+      def initialize(config)
+        @config = config
+      end
 
-    def about
-      LintRoller::About.new(
-        name: "standard-rails",
-        version: VERSION,
-        homepage: "https://github.com/testdouble/standard-rails",
-        description: "Configuration for rubocop-rails rules"
-      )
-    end
+      def about
+        LintRoller::About.new(
+          name: "standard-rails",
+          version: VERSION,
+          homepage: "https://github.com/testdouble/standard-rails",
+          description: "Configuration for rubocop-rails rules"
+        )
+      end
 
-    def supported?(context)
-      true
-    end
+      def supported?(context)
+        true
+      end
 
-    def rules(context)
-      trick_rubocop_into_thinking_we_required_rubocop_rails!
+      def rules(context)
+        trick_rubocop_into_thinking_we_required_rubocop_rails!
 
-      LintRoller::Rules.new(
-        type: :object,
-        config_format: :rubocop,
-        value: rules_with_config_applied
-      )
-    end
+        LintRoller::Rules.new(
+          type: :object,
+          config_format: :rubocop,
+          value: rules_with_config_applied
+        )
+      end
 
-    private
+      private
 
-    def rules_with_config_applied
-      YAML.load_file(Pathname.new(__dir__).join("../../../config/base.yml")).tap do |rules|
-        if @config.key?("target_rails_version")
-          rules["AllCops"]["TargetRailsVersion"] = @config["target_rails_version"]
+      def rules_with_config_applied
+        YAML.load_file(Pathname.new(__dir__).join("../../../config/base.yml")).tap do |rules|
+          if @config.key?("target_rails_version")
+            rules["AllCops"]["TargetRailsVersion"] = @config["target_rails_version"]
+          end
         end
       end
-    end
 
-    # This is not fantastic.
-    #
-    # When you `require "rubocop-rails"`, it will not only load the cops,
-    # but it will also monkey-patch RuboCop's default_configuration, which is
-    # something that can't be undone for the lifetime of the process.
-    #
-    # See: https://github.com/rubocop/rubocop-rails/blob/master/lib/rubocop-rails.rb#L14
-    #
-    # As an alternative, standard-rails loads the cops directly, and then
-    # simply tells the RuboCop config loader that it's been loaded. This is
-    # taking advantage of a private API of an `attr_reader` that probably wasn't
-    # meant to be mutated externally, but it's better than the `Inject` monkey
-    # patching that rubocop-rails does (and many other RuboCop plugins do)
-    def trick_rubocop_into_thinking_we_required_rubocop_rails!
-      require_relative "load_rubocop_rails_without_the_monkey_patch"
-      RuboCop::ConfigLoader.default_configuration.loaded_features.add("rubocop-rails")
+      # This is not fantastic.
+      #
+      # When you `require "rubocop-rails"`, it will not only load the cops,
+      # but it will also monkey-patch RuboCop's default_configuration, which is
+      # something that can't be undone for the lifetime of the process.
+      #
+      # See: https://github.com/rubocop/rubocop-rails/blob/master/lib/rubocop-rails.rb#L14
+      #
+      # As an alternative, standard-rails loads the cops directly, and then
+      # simply tells the RuboCop config loader that it's been loaded. This is
+      # taking advantage of a private API of an `attr_reader` that probably wasn't
+      # meant to be mutated externally, but it's better than the `Inject` monkey
+      # patching that rubocop-rails does (and many other RuboCop plugins do)
+      def trick_rubocop_into_thinking_we_required_rubocop_rails!
+        require_relative "load_rubocop_rails_without_the_monkey_patch"
+        RuboCop::ConfigLoader.default_configuration.loaded_features.add("rubocop-rails")
+      end
     end
   end
 end
